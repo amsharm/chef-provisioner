4a4234cf6abc980a6895930538494cd3a7b65c9e
amsharm <amansharma13@gmail.com>
2017-04-01 11:08:28 -0500

    First commit

---
+++ Master2.ps1
@@ -0,0 +1,26 @@
+﻿# This script acts as an orchestrator, making calls to a Packer template and Chef recipes
+
+# createAMI.json is a Packer template that accepts a number of parameters used to create a Windows AMI with chef client, IIS,
+# and an Octopus tentacle all configured. The output of the Packer run is saved to a file, so that I can programmatically
+# reference AMI id of the AMI just created
+packer build C:\Users\aman.f.sharma\Packer\createAMI.json | Tee-Object -filepath C:\Users\aman.f.sharma\packerlog.log
+
+# Getting the AMI id from the newly created AMI. I then surround the AMI id in a JSON object so that it's in the right format
+# for a Chef attribute and save it to an attributes file 
+$AmiID = ((Get-Content C:\Users\aman.f.sharma\packerlog.log)[-1]).Split(" ")[-1]
+$jsonString = "{AmiID:'"+$AmiID+"'}"
+$jsonObject = $jsonString | ConvertFrom-Json
+$jsonObject | ConvertTo-Json -depth 999 | Out-File -Encoding ascii "C:\Users\aman.f.sharma\chef-repo\cookbooks\provisioner\attributes.json"
+
+# Run chef and pass in the newly created AMI
+chef-client -z -j C:\Users\aman.f.sharma\chef-repo\cookbooks\provisioner\attributes.json C:\Users\aman.f.sharma\chef-repo\cookbooks\provisioner\recipes\test3.rb | Tee-Object -filepath C:\Users\aman.f.sharma\cheflog.log
+
+# As a part of the chef run, an auto scaling group is created. The unique id or ARN of the scaling group is saved to a txt file 
+# so that it can be passed in as a parameter on the second chef run below which is used to create a Cloudwatch alarm 
+# linked to the newly created auto scaling group
+$scalingGroupARN = Get-Content C:\Users\aman.f.sharma\chef-repo\cookbooks\provisioner\scalinggroup.txt
+$jsonString = "{ScalingGroupARN:'"+$scalingGroupARN+"'}"
+$jsonObject = $jsonString | ConvertFrom-Json
+$jsonObject | ConvertTo-Json -depth 999 | Out-File -Encoding ascii "C:\Users\aman.f.sharma\chef-repo\cookbooks\provisioner\scalinggroup.json"
+Start-Sleep -s 1500
+chef-client -z -j C:\Users\aman.f.sharma\chef-repo\cookbooks\provisioner\scalinggroup.json C:\Users\aman.f.sharma\chef-repo\cookbooks\provisioner\recipes\cloudwatch.rb 
---
+++ instancesetup.ps1
@@ -0,0 +1,148 @@
+﻿# Load AWS SDK assemblies
+Add-Type -path 'C:\Program Files (x86)\AWS Tools\PowerShell\AWSPowerShell\AWS*.dll'
+
+# The ID of the stack that I'm testing with. Eventually this will be programmatically set
+$stackId = "224b49c1-3fc2-4e4d-8371-963c7a8c3ac5"
+
+# Create the OpsWorks client 
+$opsworksClient = New-Object Amazon.OpsWorks.AmazonOpsWorksClient([Amazon.RegionEndpoint]::USEast1)
+
+# Create and customize the layer request object. Part of the customization for this is adding a recipe that installs the Octopus Deploy tentacle to the layer setup event
+$layerReq = New-Object Amazon.OpsWorks.Model.CreateLayerRequest
+$layerType = New-Object Amazon.OpsWorks.LayerType("Custom")
+$layerRecipes = New-Object Amazon.OpsWorks.Model.Recipes
+$configureRecipesList = New-Object System.Collections.Generic.List[System.String]
+$configureRecipesList.Add("installoctopus::default")
+$layerRecipes.Setup = $configureRecipesList
+$layerReq.Name = "PerfAchievementLayer"
+$layerReq.StackId = $stackId
+$layerReq.Type = $layerType
+$layerReq.Shortname = "perfach"
+$layerReq.CustomRecipes = $layerRecipes
+$createLayerResponse = $opsworksClient.CreateLayer($layerReq)
+$layerId = $createLayerResponse.LayerId
+
+# Create and customize the instance request object
+$instanceReq = New-Object Amazon.OpsWorks.Model.CreateInstanceRequest
+$instanceReq.StackId = $stackId 
+$instanceReq.LayerIds = $layerId
+$instanceReq.InstanceType="t2.micro"
+
+# Create two instances and store their instance ids
+$instanceReqResponse = $opsworksclient.CreateInstance($instanceReq)
+$instanceId_1 = $instanceReqResponse.InstanceId
+$instanceReqResponse = $opsworksclient.CreateInstance($instanceReq)
+$instanceId_2 = $instanceReqResponse.InstanceId
+
+# Start the instances
+$startInstanceReq_1 = New-Object Amazon.OpsWorks.Model.StartInstanceRequest
+$startInstanceReq_2 = New-Object Amazon.OpsWorks.Model.StartInstanceRequest
+$startInstanceReq_1.InstanceId = $instanceId_1
+$startInstanceReq_2.InstanceId = $instanceId_2
+$opsworksClient.StartInstance($startInstanceReq_1)
+$opsworksClient.StartInstance($startInstanceReq_2)
+
+# Wait until instances are online. Reason for this is that we cannot attach the ELB until the instances are online
+$instancesOnlineFlag = 0
+$instanceIds = New-Object System.Collections.Generic.List[System.String]
+$instanceIds.Add($instanceId_1)
+$instanceIds.Add($instanceId_2)
+$describeInstancesReq = New-Object Amazon.OpsWorks.Model.DescribeInstancesRequest
+$describeInstancesReq.InstanceIds = $instanceIds
+$sw = [system.diagnostics.stopwatch]::startNew()
+while ($instancesOnlineFlag -eq 0){
+    $instancesOnlineFlag = 1
+    $describeInstancesResponse = $opsworksClient.DescribeInstances($describeInstancesReq)
+    $instancesResponseList = $describeInstancesResponse.Instances
+    foreach ($instance in $instancesResponseList){
+        if($instance.Status -ne "online") {
+            Write-Host "Instance status: "$instance.Status`n
+            $instancesOnlineFlag = 0
+            break
+        }
+    }
+    if($instancesOnlineFlag -eq 1){
+       break
+    }
+    else{
+        Write-Host "The instances are currently in the process of starting up. Please wait... `nCurrent elapsed time: "
+        Write-Host $sw.Elapsed `n `n
+    }
+    Start-Sleep -s 15
+}
+
+$sw.Stop()
+# Create the ELB client
+$elbClient = New-Object Amazon.ElasticLoadBalancing.AmazonElasticLoadBalancingClient([Amazon.RegionEndpoint]::USEast1)
+
+# Create and customize the ELB request object
+$elbReq = New-Object Amazon.ElasticLoadBalancing.Model.CreateLoadBalancerRequest
+$listener = New-Object Amazon.ElasticLoadBalancing.Model.Listener("HTTP",80,80)
+$elbReq.LoadBalancerName="testELB"
+$elbReq.Listeners = $listener
+$elbReq.Subnets = "subnet-0e8c5556"
+
+# Create the ELB
+$elbClient.CreateLoadBalancer($elbReq)
+
+# Now, we will attach the instances to the ELB, but first we need to store the instance ids in the right format
+$instanceList = New-Object System.Collections.Generic.List[Amazon.ElasticLoadBalancing.Model.Instance]
+$instanceContainer_1 = New-Object Amazon.ElasticLoadBalancing.Model.Instance
+$instanceContainer_2 = New-Object Amazon.ElasticLoadBalancing.Model.Instance
+$describeInstancesResponse = $opsworksClient.DescribeInstances($describeInstancesReq)
+$instancesResponseList = $describeInstancesResponse.Instances
+$instanceContainer_1.InstanceId = $instancesResponseList.Item(0).Ec2InstanceId
+$instanceContainer_2.InstanceId = $instancesResponseList.Item(1).Ec2InstanceId
+$instanceList.Add($instanceContainer_1)
+$instanceList.Add($instanceContainer_2)
+
+# Create and customize the request object that is used to attach the instances to the elb
+$registerInstancesReq = New-Object Amazon.ElasticLoadBalancing.Model.RegisterInstancesWithLoadBalancerRequest
+$registerInstancesReq.Instances = $instanceList
+$registerInstancesReq.LoadBalancerName = "testELB"
+
+# Attach the instances to the ELB
+$elbClient.RegisterInstancesWithLoadBalancer($registerInstancesReq)
+
+# Create the DynamoDB client
+$dynamoClient = New-Object Amazon.DynamoDBv2.AmazonDynamoDBClient([Amazon.RegionEndpoint]::USEast1)
+
+# Create and customize the table request object
+$tableReq = New-Object Amazon.DynamoDBv2.Model.CreateTableRequest
+$tableReq.TableName = "PerformanceAchievement"
+
+# Specify the attribute definitions for this table
+$attributesList = New-Object System.Collections.Generic.List[Amazon.DynamoDBv2.Model.AttributeDefinition]
+$stringAttributeType = New-Object Amazon.DynamoDBv2.ScalarAttributeType("S")
+$careerLevelAttribute = New-Object Amazon.DynamoDBv2.Model.AttributeDefinition
+$employeeNameAttribute = New-Object Amazon.DynamoDBv2.Model.AttributeDefinition
+$careerLevelAttribute.AttributeName = "Career Level"
+$careerLevelAttribute.AttributeType = $stringAttributeType
+$employeeNameAttribute.AttributeName = "Employee Name"
+$employeeNameAttribute.AttributeType = $stringAttributeType
+$attributesList.Add($careerLevelAttribute)
+$attributesList.Add($employeeNameAttribute)
+$tableReq.AttributeDefinitions = $attributesList
+
+# Specify the Key Schema list
+$keySchemaList = New-Object System.Collections.Generic.List[Amazon.DynamoDBv2.Model.KeySchemaElement]
+$hashKeyType = New-Object Amazon.DynamoDBv2.KeyType("HASH")
+$rangeKeyType = New-Object Amazon.DynamoDBv2.KeyType("RANGE")
+$careerLevelKeyElement = New-Object Amazon.DynamoDBv2.Model.KeySchemaElement
+$employeeNameKeyElement = New-Object Amazon.DynamoDBv2.Model.KeySchemaElement
+$careerLevelKeyElement.AttributeName = "Career Level"
+$careerLevelKeyElement.KeyType = $hashKeyType
+$employeeNameKeyElement.AttributeName = "Employee Name"
+$employeeNameKeyElement.KeyType = $rangeKeyType
+$keySchemaList.add($careerLevelKeyElement)
+$keySchemaList.add($employeeNameKeyElement)
+$tableReq.KeySchema = $keySchemaList
+
+# Specify the provisioned throughput
+$provisionedThroughput = New-Object Amazon.DynamoDBv2.Model.ProvisionedThroughput
+$provisionedThroughput.ReadCapacityUnits = 20
+$provisionedThroughput.WriteCapacityUnits = 50
+$tableReq.ProvisionedThroughput = $provisionedThroughput
+
+# Create the DynamoDB table
+$dynamoClient.CreateTable($tableReq)
